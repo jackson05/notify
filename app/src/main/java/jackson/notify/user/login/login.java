ac7dbb1ab427a188139d956a83c0902696718a4e
jackson05 <jackson5g.bi@gmail.com>
2024-03-09 22:57:49 +0100

    login view added

---
+++ app/src/main/java/jackson/notify/user/login/login.java
@@ -0,0 +1,16 @@
+package jackson.notify.user.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import jackson.notify.R;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
